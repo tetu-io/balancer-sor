1a779383c9744ef8d59db557df85fd013ddb5906
Viacheslav Bogdanov <viacheslav.v.bogdanov@gmail.com>
2022-09-05 16:03:55 +0700

    Disable all dexes except Dystopia & TetuSwap

--- test/api/api.ts
+++ test/api/api.ts
@@ -109,7 +109,7 @@ export async function init(
         );
 
     const subgraphPoolDataServices: PoolDataService[] = [
-        subgraphBalancerPoolDataService,
+        // subgraphBalancerPoolDataService,
         subgraphDystopiaPoolDataService,
         ...subgraphUniswapPoolDataServices,
     ];
